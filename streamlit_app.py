09d4b87fe3cb291094e7064715e85e6168241c2d
andihariandi4232401054 <andi.4232401054@students.polibatam.ac.id>
2025-12-28 10:46:04 +0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,47 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.title("Data Diri Mahasiswa")
+st.header("Profil Pribadi")
+st.subheader("Informasi Umum")
+st.caption("Contoh web page Streamlit bertema data diri")
+st.text(
+    "Halaman ini menampilkan data diri saya (Andi Hariandi) mahasiswa Jurusan Elektro, Prodi Rekayasa Pembangkit Energi "
+    "menggunakan beberapa komponen dasar pada Streamlit."
 )
+st.subheader("Contoh Kode Streamlit")
+st.code("""
+st.title("Data Diri Mahasiswa")
+st.write("Andi Hariandi, 4232401054, dan Rekayasa Pembangkit Energi")
+""", language="python")
+st.subheader("Data Diri")
+data_diri = {
+    "Keterangan": [
+        "Nama                    :",
+        "NIM                        :",
+        "Program Studi  :",
+        "Vokasi                  :",
+        "Politeknik:         :",
+        "Tahun Masuk    :"
+    ],
+    "Data": [
+        "Andi Hariandi",
+        "4232401054",
+        "Teknik Elektro",
+        "Teknik",
+        "Politeknik Negeri Batam",
+        "2024"
+    ]
+}
+df = pd.DataFrame(data_diri)
+st.dataframe(df, use_container_width=True)
+st.subheader("Visualisasi Aktivitas Akademik")
+
+aktivitas = {
+    "Semester": ["Semester 1", "Semester 2", "Semester 3"],
+    "Jumlah SKS": [17, 19, 20,]
+}
+
+df_chart = pd.DataFrame(aktivitas).set_index("Semester")
+
+st.line_chart(df_chart)
